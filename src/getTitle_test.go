caa16ff1ab9eed4fe6b128d5573965894757195d
gmgale <gmgale@icloud.com>
2020-11-02 16:53:44 +0100

    Test update

--- src/getTitle_test.go
+++ src/getTitle_test.go
@@ -2,8 +2,9 @@ package main
 
 import "testing"
 
-// TestGetTitle is a unit test to simulate itterations of user inputted
-// threadsfrom 1 to len(urls).
+// TestGetTitle is a unit test to simulate iterations of user inputted
+// threadsfrom 1 to len(urls) and comfirm correct ammount of titles are
+// returned.
 func TestGetTitle(t *testing.T) {
 	for i := 1; i <= len(urls); i++ {
 		threads := i
@@ -13,6 +14,20 @@ func TestGetTitle(t *testing.T) {
 		}
 		if len(titles) > len(urls) {
 			t.Errorf("%d titles returned, expected %d.", len(titles), len(urls))
+		}
+	}
+}
+
+// TestGetTitleStatusCount is a unit test to simulate iterations of user inputted
+// threads from 1 to len(urls) and comfirm correct number of status (succeed/fail)
+// calls count is returned.
+func TestGetTitleStatusCount(t *testing.T) {
+	for i := 1; i <= len(urls); i++ {
+		threads := i
+		_, status := getTitle(threads)
+		if (status[0] + status[1]) != len(urls) {
+			t.Errorf("%d succeeded and %d failed responces returned totaling %d, expected a total of %d.",
+				status[0], status[1], (status[0] + status[1]), len(urls))
 		}
 	}
 }
---
+++ src/parseHTML_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestParseHTML(t *testing.T) {
+
+}
