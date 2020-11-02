ed2cca049c20840df808f769adca0fffefa29e60
gmgale <gmgale@icloud.com>
2020-11-02 16:35:44 +0100

    test for getTitle func completed

--- src/getTitle_test.go
+++ src/getTitle_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-// TestGetTitle1 is a unit test to simulate itterations of user inputted
+// TestGetTitle is a unit test to simulate itterations of user inputted
 // threadsfrom 1 to len(urls).
-func TestGetTitle1(t *testing.T) {
+func TestGetTitle(t *testing.T) {
 	for i := 1; i <= len(urls); i++ {
 		threads := i
 		titles, _ := getTitle(threads)
@@ -13,5 +13,6 @@ func TestGetTitle1(t *testing.T) {
 		}
 		if len(titles) > len(urls) {
 			t.Errorf("%d titles returned, expected %d.", len(titles), len(urls))
+		}
 	}
 }
