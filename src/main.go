9f02a77f25426a36e69215fc9a3f6d43952a5011
gmgale <gmgale@icloud.com>
2020-11-03 20:34:42 +0100

    Moved channels to seperate file

--- src/main.go
+++ src/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "sync"
-
 const (
 	statusSucceeded = "succeeded"
 	statusFailed    = "failed"
@@ -15,20 +13,9 @@ var urls = [...]string{
 	"https://www.result.si/blog/",
 }
 
-// urlCh is a channel for title data.
-var urlCh = make(chan string)
-
-// statCh is a channel for GET url succ/fail counter.
-var statCh = make(chan string)
-
-// wg is the WaitGroup used for batch processing Goroutines
-// in getTitle and parseHTML functions
-var wg sync.WaitGroup
-
 func main() {
 
-	defer close(urlCh)
-	defer close(statCh)
-
 	startServer()
+
+	closeChannels()
 }
