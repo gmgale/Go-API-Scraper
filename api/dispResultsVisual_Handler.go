34d4411ebaffcbfc1d5f6776c833a2cc75615b08
gmgale <gmgale@icloud.com>
2020-11-12 21:59:44 +0100

    results-visual not yet working

--- api/dispResultsVisual_Handler.go
+++ api/dispResultsVisual_Handler.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+
 	"encoding/json"
-	"fmt"
 	"github.com/thedevsaddam/renderer"
 	"log"
 	"net/http"
@@ -20,17 +20,23 @@ type localDataStr struct {
 
 var rnd *renderer.Render
 
-// dispResults is a function to pull data from the database
-// and then print to the browser.
+func init() {
+	opts := renderer.Options{
+		ParseGlobPattern: "./web/*.html",
+	}
+	rnd = renderer.New(opts)
+}
+
+// dispResultsVisual is a function to pull data from the database
+// and then print to the browser in formatted HTML.
 func dispResultsVisual(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain")
+	w.Header().Add("Content-Type", "html")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(http.StatusOK)
 
 	rows, _ := db.Query(`SELECT id, data FROM calls;`)
 
 	// dbLocal will be a local pull of the database
-	var dbLocal []localDataStr
+	var DBLocal []localDataStr
 
 	for rows.Next() {
 		var row rowStr
@@ -49,21 +55,11 @@ func dispResultsVisual(w http.ResponseWriter, r *http.Request) {
 			var x localDataStr
 			x.id = row.id
 			x.data = newTitleData
-			dbLocal = append(dbLocal, x)
+			DBLocal = append(DBLocal, x)
 		}
 	}
-
-	for i := 0; i < len(dbLocal); i++ {
-		fmt.Fprintln(w, "ID: ", dbLocal[i].id)
-		fmt.Fprintln(w, dbLocal[i].data)
-	}
-
-	opts := renderer.Options{
-		ParseGlobPattern: "./api/pages/*.html",
-	}
-	rnd = renderer.New(opts)
-
-	rnd.HTML(w, http.StatusOK, "results", nil)
-
 	rows.Close()
+	log.Println("Data unmarshalled.")
+
+	rnd.HTML(w, http.StatusOK, "results", DBLocal)
 }
